4404cf7203d2749fc7a2ccdc663d65615e5fa21a
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2023-03-30 12:17:19 +0200

    Add folia support

--- src/main/kotlin/net/minecrell/pluginyml/bukkit/BukkitPluginDescription.kt
+++ src/main/kotlin/net/minecrell/pluginyml/bukkit/BukkitPluginDescription.kt
@@ -53,6 +53,7 @@ class BukkitPluginDescription(project: Project) : PluginDescription {
     @Input @Optional @JsonProperty("default-permission") var defaultPermission: Permission.Default? = null
     @Input @Optional var provides: List<String>? = null
     @Input @Optional var libraries: List<String>? = null
+    @Input @Optional @JsonProperty("folia-supported") var foliaSupported: Boolean? = null
 
     @Nested val commands: NamedDomainObjectContainer<Command> = project.container(Command::class.java)
     @Nested val permissions: NamedDomainObjectContainer<Permission> = project.container(Permission::class.java)

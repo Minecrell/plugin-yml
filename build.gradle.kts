f66683d1820856d912d12c84a923431418e56c54
Minecrell <minecrell@minecrell.net>
2023-06-19 09:12:51 +0200

    build: Update jackson to 2.15.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,10 @@ repositories {
 }
 
 dependencies {
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2") {
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2") {
         exclude(group = "org.jetbrains.kotlin")
     }
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.2")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.2")
 }
 
 gradlePlugin {

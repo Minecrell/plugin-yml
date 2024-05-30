42dec2fea54ce40a44beb98ce880e3f7bc38197c
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2024-05-30 10:47:54 +0200

    Add validation for main class and api version

--- src/main/kotlin/net/minecrell/pluginyml/bukkit/BukkitPlugin.kt
+++ src/main/kotlin/net/minecrell/pluginyml/bukkit/BukkitPlugin.kt
@@ -27,13 +27,21 @@ package net.minecrell.pluginyml.bukkit
 import net.minecrell.pluginyml.InvalidPluginDescriptionException
 import net.minecrell.pluginyml.PlatformPlugin
 import net.minecrell.pluginyml.collectLibraries
+import net.minecrell.pluginyml.paper.PaperPlugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.result.ResolvedComponentResult
 
 class BukkitPlugin : PlatformPlugin<BukkitPluginDescription>("Bukkit", "plugin.yml") {
 
     companion object {
-        @JvmStatic private val VALID_NAME = Regex("^[A-Za-z0-9 _.-]+$")
+        @JvmStatic
+        private val VALID_NAME = Regex("^[A-Za-z0-9 _.-]+$")
+
+        @JvmStatic
+        private val VALID_API_VERSION = Regex("^1\\.[0-9]+$")
+
+        @JvmStatic
+        private val INVALID_NAMESPACES = listOf("net.minecraft.", "org.bukkit.", "io.papermc.", "com.destroystokoyo.paper.", "org.spigotmc")
     }
 
     override fun createExtension(project: Project) = BukkitPluginDescription(project)
@@ -53,12 +61,17 @@ class BukkitPlugin : PlatformPlugin<BukkitPluginDescription>("Bukkit", "plugin.y
     override fun validate(description: BukkitPluginDescription) {
         val name = description.name ?: throw InvalidPluginDescriptionException("Plugin name is not set")
         if (!VALID_NAME.matches(name)) throw InvalidPluginDescriptionException("Invalid plugin name: should match $VALID_NAME")
+        if (description.apiVersion != null) {
+            val apiVersion = description.apiVersion!!
+            if (!VALID_API_VERSION.matches(apiVersion)) throw InvalidPluginDescriptionException("Invalid api version: should match $VALID_API_VERSION")
+            if (apiVersion < "1.13") throw InvalidPluginDescriptionException("Invalid api version: should be at least 1.13")
+        }
 
         if (description.version.isNullOrEmpty()) throw InvalidPluginDescriptionException("Plugin version is not set")
 
         val main = description.main ?: throw InvalidPluginDescriptionException("Main class is not defined")
         if (main.isEmpty()) throw InvalidPluginDescriptionException("Main class cannot be empty")
-        if (main.startsWith("org.bukkit.")) throw InvalidPluginDescriptionException("Main may not be within the org.bukkit namespace")
+        validateNamespace(main, "Main")
 
         for (command in description.commands) {
             if (command.name.contains(':')) throw InvalidPluginDescriptionException("Command '${command.name}' cannot contain ':'")
@@ -72,4 +85,11 @@ class BukkitPlugin : PlatformPlugin<BukkitPluginDescription>("Bukkit", "plugin.y
         }
     }
 
+    private fun validateNamespace(namespace: String, name: String) {
+        for (invalidNamespace in INVALID_NAMESPACES) {
+            if (namespace.startsWith(invalidNamespace)) {
+                throw InvalidPluginDescriptionException("$name may not be within the $invalidNamespace namespace")
+            }
+        }
+    }
 }
